161181956e46f65dfcea76c3dd9f8a9e288817b6
clril <clril@doge>
2015-10-13 00:18:31 +0200

    Maj FilterRest. Modify headers

--- src/main/java/org/lamop/riche/webservices/FilterRest.java
+++ src/main/java/org/lamop/riche/webservices/FilterRest.java
@@ -12,13 +12,15 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * Filter used for all restService. Add Access-Control-Allow-Origin for test
+ *
  * @author clril
  */
-public class FilterRest implements Filter{
+public class FilterRest implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -28,21 +30,25 @@ public class FilterRest implements Filter{
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-     HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8383");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
-        
-        chain.doFilter(req, res);
-    
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        response.setHeader("Access-Control-Request-Headers", "X-Requested-With, accept, content-type");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,  X-Requested-With, X-CSRF-Token, X-Requested-With, Content-Type, Accept");
+        HttpServletRequest request = (HttpServletRequest) req;
+        if (!request.getMethod().equals("OPTIONS")) {
+            chain.doFilter(req, res);
+        } else {
+            System.out.println("LA OPTIONS");
+        }
+
     }
 
     @Override
     public void destroy() {
-        
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
