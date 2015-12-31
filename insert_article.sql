7949ff6e179e72dcb44e8e1cb5aab439a6c6d550
Clement <clril@smile.fr>
2015-12-31 20:03:40 +0100

    Un simple script sql pour introduire les articles de description du projet.

---
+++ insert_article.sql
@@ -0,0 +1,8 @@
+/**
+La description et l'historique du projet sont enregistré en base de données. 
+Il est nécessaire d'introduire les premiers articles lors de l'installation de 
+l'application
+**/
+
+INSERT INTO Article (id, content, title) VALUES(1, "titre article de description du projet", "Contenu de l'article");
+INSERT INTO Article (id, content, title) VALUES(2, "titre article de d'historique", "Contenu de l'article");
